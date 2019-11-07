e06b7ac92219a5ea3444442ffff258f2154c4396
Nick Solonyy <nicksolony@gmail.com>
2019-11-07 06:32:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line (order)
       order.each_with_index  do |name,i|
       numbered_line << "#{i+1}. #{name}"
       end
-    puts "The line is currently: #{numbered_line.join}"
+    puts "The line is currently: #{numbered_line.join (" ")}"
     
     end
   
