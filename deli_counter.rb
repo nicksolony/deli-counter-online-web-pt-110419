223a5d09b28c877946910ad34d43a5fbcbaef952
Nick Solonyy <nicksolony@gmail.com>
2019-11-07 06:31:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,14 @@
 katz_deli = []
 
 def line (order)
+    numbered_line = []
     if order.length == 0
       puts "The line is currently empty."
     else
-    order.each_with_index  do |name,i|
-      puts "#{i+1}. #{name}"
+      order.each_with_index  do |name,i|
+      numbered_line << "#{i+1}. #{name}"
+      end
+    puts ""
     end
-  end
   
 end
